2d2150c1a23c24fafdb9ea393325675f4aab6316
marcelo0137 <marcelovictor0137@gmail.com>
2023-03-14 01:40:41 -0300

    first commit

--- test/example/TriangleTest.java
+++ test/example/TriangleTest.java
@@ -8,50 +8,50 @@ import org.junit.jupiter.api.Test;
 public class TriangleTest { 
 	 
     @Test 
-    public void equilateralTriangleHaveEqualSides() throws TriangleException { 
+    public void equilateralEqualSides() throws TriangleException { 
         Triangle triangle = new Triangle(2, 2, 2);  // Triângulo Equilatero - Entrada 2,2,2
  
         assertEquals(TriangleKind.EQUILATERAL, triangle.getKind()); 
     }
     @Test
-    public void scaleneHasNoEqualSides() throws TriangleException {
+    public void scaleneDifferentSides() throws Exception {
     	Triangle triangle = new Triangle (5,6,7); // Triângulo Escaleno - Entrada 5,6,7
     	assertEquals(TriangleKind.SCALENE,triangle.getKind());
     }
     @Test
-    public void isoscelesTriangleTwoEqualSides() throws TriangleException {
+    public void isoscelesTwoSides() throws Exception {
     	Triangle triangle = new Triangle(3,3,2); // Triângulo Isosceles - Entrada 3,3,2
     	assertEquals(TriangleKind.ISOSCELES,triangle.getKind());
     }
     @Test
-    public void equilateralTriangleUniqueSide() throws TriangleException { // Triângulo Equilatero - Verifica o Número de Lados Únicos
+    public void equilateralTriangleOneSide() throws Exception { // Triângulo Equilatero - Verifica o Número de Lados Únicos
     	Triangle triangle = new Triangle (2,2,2);
-        int ladosDiferentes = 1;
-        assertEquals(ladosDiferentes,triangle.getNumberOfUniqueSides()); // Compara valor esperado com o valor atual	
+    	int numberOfUniqueSides = triangle.getNumberOfUniqueSides(); 
+    	assertTrue(numberOfUniqueSides == 1); // Verifica se condição é verdadeira
     }
     @Test
-    public void scaleneTriangleThreeSides() throws TriangleException { // Triângulo Escaleno - Verifica o Número de Lados Únicos
+    public void scaleneTriangleThreeSides() throws Exception { // Triângulo Escaleno - Verifica o Número de Lados Únicos
     	Triangle triangle = new Triangle (5,6,7);
-    	int ladosDiferentes = 3;
-    	assertEquals(ladosDiferentes,triangle.getNumberOfUniqueSides()); 
+    	int numberOfUniqueSides = triangle.getNumberOfUniqueSides();
+    	assertTrue(numberOfUniqueSides == 3);
     	
     }
     @Test
-    public void isoscelesTriangleTwoSides() throws TriangleException { // Triângulo Isoceles - Verifica o Número de Lados Únicos
+    public void isoscelesTriangleTwoSides() throws Exception { // Triângulo Isoceles - Verifica o Número de Lados Únicos
     	Triangle triangle = new Triangle(3,3,2);
-    	int ladosDiferentes = 2;
-    	assertEquals(ladosDiferentes,triangle.getNumberOfUniqueSides());
+    	int numberOfUniqueSides = triangle.getNumberOfUniqueSides();
+    	assertTrue(numberOfUniqueSides == 2);
     }
     @Test
-    public void triangleSidesEqualZero() { // Triângulo Lados Iguais a 0
+    public void triangleSidesZero() { // Triângulo Lados Iguais a 0
     	Assertions.assertThrows(TriangleException.class, ()->{new Triangle(0,0,0);});// Classe Assertion método assertThrows
     }
     @Test
-    public void triangleSidesNegative() { // Triângulos Lados Negativos/ lados menores que 0
+    public void triangleImpossibleSides() { // Triângulos Lados Negativos - lados menores que 0
     	Assertions.assertThrows(TriangleException.class,()->{new Triangle(-5,-6,-7);});
    }
     @Test
-    public void triangleInequality() { // Desigualdade de Lados
+    public void triangleInequality() { // Desigualdade de Lados - Não é possível formar um triânguloe
     	Assertions.assertThrows(TriangleException.class,()->{new Triangle(2,2,5);});
     }
     
